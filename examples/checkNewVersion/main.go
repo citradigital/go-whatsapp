d9e7709346ca6003554c7107faabbce6eb492552
Herpiko Dwi Aguno <herpiko@gmail.com>
2020-11-04 14:02:28 +0700

    Replace namespace to citradigital.

--- examples/checkNewVersion/main.go
+++ examples/checkNewVersion/main.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/Rhymen/go-whatsapp"
+	"github.com/citradigital/go-whatsapp"
 )
 
 type requestBody struct {
--- handler.go
+++ handler.go
@@ -5,8 +5,8 @@ import (
 	"os"
 	"strings"
 
-	"github.com/Rhymen/go-whatsapp/binary"
-	"github.com/Rhymen/go-whatsapp/binary/proto"
+	"github.com/citradigital/go-whatsapp/binary"
+	"github.com/citradigital/go-whatsapp/binary/proto"
 )
 
 /*
